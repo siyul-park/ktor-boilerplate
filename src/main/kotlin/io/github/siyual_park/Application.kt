7039ff76581d3b0c193a122d7de807cfc3c3c145
siyual-park <kdpark0723@gmail.com>
2021-04-02 17:29:19 +0900

    routes 파일 분리

--- src/main/kotlin/io/github/siyual_park/Application.kt
+++ src/main/kotlin/io/github/siyual_park/Application.kt
@@ -1,18 +1,16 @@
 package io.github.siyual_park
 
+import io.github.siyual_park.routes.getRootRoutes
 import io.ktor.application.Application
-import io.ktor.application.call
-import io.ktor.response.respondText
+import io.ktor.application.install
+import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.routing
 import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
-    routing {
-        get("/") {
-            call.respondText("Hello, world!")
-        }
+    install(Routing) {
+        getRootRoutes()
     }
 }
